6a24e017fcce92e0603d3bc51b6fd1818786a1c6
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-06-05 17:12:22 -0300

    Add files via upload

--- feed.php
+++ feed.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,11 +14,14 @@
         <div class="container-fluid">
             <a class="navbar-brand">Navbar</a>
             <div class="d-flex">
-                <form class="d-flex" role="search">
+                <form class="me-2" role="search">
                     <input class="form-control me-2" type="search" placeholder="Pesquisar..." aria-label="Search">
                 </form>
-                <button class="btn btn-outline-success">
+                <button class="btn btn-outline-success me-2">
                     <i class="fa fa-user"></i>
+                </button>
+                <button id="switchTheme" class="btn btn-outline-secondary">
+                    <i id="switchThemeIcon" class="fa fa-sun"></i>
                 </button>
             </div>
         </div>
@@ -26,19 +29,38 @@
 
     <div class="row m-4 justify-content-center">
         <!-- NAVEGAÇÃO HORIZONTAL -->
-        <div class="col-3">
-            <div class="container card"></div>
+        <div class="col-0 col-md-3">
+            <div class="card p-4">
+                <nav class="nav nav-pills flex-column">
+                    <a class="my-2 nav-link active" href="#">
+                        <i class="fa fa-feed"></i>
+                        Feed
+                    </a>
+                    <a class="my-2 nav-link" href="#">
+                        <i class="fa fa-user"></i>
+                        Meu Perfil
+                    </a>
+                    <a class="my-2 nav-link" href="#">
+                        <i class="fa fa-gear"></i>
+                        Configurações
+                    </a>
+                </nav>
+            </div>
         </div>
 
         <!-- FEED -->
-        <div class="col-4">
+        <div class="col-12 col-md-4">
             
             <div class="card mb-4">
                 <div class="card-header">Titulo</div>
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-footer">
                     <p class="card-text">Texto Texto Texto</p>
-                    <p class="card-text"><small class="text-body-secondary">05/06/2023 - 15:50</small></p>
+                    <p class="card-text d-flex">
+                        <button class="btn btn-sm btn-outline-danger me-1">C</button>
+                        <button class="btn btn-sm btn-outline-success">C</button>
+                        <small class="text-body-secondary ms-auto">05/06/2023 - 15:50</small>
+                    </p>
                 </div>
             </div>
 
@@ -63,14 +85,62 @@
         </div>
 
         <!-- SIDEBAR -->
-        <div class="col-3">
-            <div class="card container">
-                
+        <div class="col-12 col-md-3">
+            <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between">
+                    <h6>S</h6>   
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between">
+                    <h6>#Hastags</h6>   
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">#Teste</li>
+                    <li class="list-group-item">#Teste</li>
+                    <li class="list-group-item">#Teste</li>
+                </ul>
+            </div>
+
+            <div class="card">
+                <div class="card-header d-flex justify-content-between">
+                    <h6>Amigos (5 Online)</h6>
+                    <a href="" class="link-underline link-underline-opacity-0">Ver todos...</a>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item px-2">
+                        
+                        <div class="d-inline-block position-relative">
+                            <img src="https://placehold.co/40x40" class="rounded" alt="">
+                            <span class="position-absolute top-10 start-100 translate-middle p-1 bg-success border border-light rounded-circle"></span>
+                        </div>
+                        Nome
+                    </li>
+                    <li class="list-group-item px-2">
+                        <div class="d-inline-block position-relative">
+                            <img src="https://placehold.co/40x40" class="rounded" alt="">
+                            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-success border border-light rounded-circle"></span>
+                        </div>
+                        Nome
+                    </li>
+                    <li class="list-group-item px-2">
+                        <div class="d-inline-block position-relative">
+                            <img src="https://placehold.co/40x40" class="rounded" alt="">
+                            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle"></span>
+                        </div>
+                        Nome
+                    </li>
+                </ul>
             </div>
         </div> 
     </div>
 
 
 </body>
+<script src="nav.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
