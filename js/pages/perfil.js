e1cb355b556e2a271eea810e4d7bba4aafda5f6c
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-06-07 17:03:14 -0300

    Add files via upload

--- js/pages/perfil.js
+++ js/pages/perfil.js
@@ -5,14 +5,18 @@ $("#username").html('@'+user);
 /* COMUNICAÇÃO COM BACKEND */
 $.ajax({
     type: "POST",
-    url: "php/loadUser.php",
+    url: "php/getPerfil.php",
     dataType: "json",
     data: {
-        user: user
+        user: user,
+        pass: 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'
     },
     success: function(response) {
         $("#name").html(response.name);
+        $("#user").html(user);
         $("#avatar").attr("src", response.avatar);
         $("#banner").attr("src", response.banner);
+
+        console.log('a'+response.name)
     }
 });
