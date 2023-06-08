67bcc288212823ad598269a90d8baa1dcb27a641
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-06-08 16:35:42 -0300

    08-06

--- js/pages/nav.js
+++ js/pages/nav.js
@@ -1,12 +1,59 @@
-document.getElementById('switchTheme').addEventListener('click',()=>{
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
-        document.getElementById('switchThemeIcon').classList.add('fa-moon');
-        document.getElementById('switchThemeIcon').classList.remove('fa-sun');
+import {
+    getCookie
+} from "../others/cookie.js";
+
+const token = getCookie('token');
+if (token != '' && token != null) {
+    $.ajax({
+        type: "POST",
+        url: "php/auth.php",
+        dataType: "json",
+        data: {
+            token: token
+        },
+        success: function(response) {
+            if (response.auth == true) {
+                window.location.href = 'home.html';
+            }
+        }
+    });
+}
+
+/* ------------------------------- */
+
+$('#switchTheme').click(function() {
+    if ($('html').attr('data-bs-theme') == 'dark') {
+        $('html').attr('data-bs-theme', 'light');
+        $('#switchThemeIcon').removeClass('fa-sun').addClass('fa-moon');
+    } else {
+        $('html').attr('data-bs-theme', 'dark');
+        $('#switchThemeIcon').removeClass('fa-moon').addClass('fa-sun');
     }
-    else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
-        document.getElementById('switchThemeIcon').classList.add('fa-sun');
-        document.getElementById('switchThemeIcon').classList.remove('fa-moon');
-    }
-})
+});
+
+$('#loginForm').submit(function() {
+    var user = $('input[name="user"]');
+    var pass = $('input[name="pass"]');
+
+    /* COMUNICAÇÃO COM BACKEND */
+    $.ajax({
+        type: "POST",
+        url: "php/login.php",
+        dataType: "json",
+        data: {
+            user: user.val(),
+            pass: pass.val()
+        },
+        success: function(response) {
+            if (response.auth == true) {
+                document.cookie = "token="+response.token;
+                user.addClass('is-valid').removeClass('is-invalid');
+                pass.addClass('is-valid').removeClass('is-invalid');
+            } else {
+                user.addClass('is-invalid').removeClass('is-valid');
+                pass.addClass('is-invalid').removeClass('is-valid');
+            }
+        }
+    });
+    return false;
+});
--- php/getPerfil.php
+++ php/getPerfil.php
@@ -27,5 +27,6 @@
 
     header('Content-Type: application/json');
     echo json_encode($response);
+    $con->close();
     exit;
 ?>
