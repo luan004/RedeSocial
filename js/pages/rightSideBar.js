3f4282b25562def0c5fddd5840e8a4366d422265
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-06-18 19:07:43 -0300

    18-06

--- js/pages/rightSideBar.js
+++ js/pages/rightSideBar.js
@@ -16,3 +16,26 @@ $.ajax({
         }
     }
 });
+
+$.ajax({
+    type: "POST",
+    url: "php/getSomeUsers.php",
+    dataType: "json",
+    success: function(response) {
+        if (response.count > 0) {
+            var num = 1;
+            while (num < response.count+1) {
+                const user = response['u'+num];
+                $("#whoFollow").append(`
+                    <a href="profile?u=${user.user}" class="list-group-item px-2">
+                        <div class="d-inline-block position-relative">
+                            <img src="${user.avatar}" width="40" height="40" class="rounded" alt="">
+                        </div>
+                        ${user.name}
+                    </a>
+                `);
+                num++;
+            }
+        }
+    }
+});
