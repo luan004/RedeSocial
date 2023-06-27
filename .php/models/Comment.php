d9ec1b50b3adb1f50c605c52fcc4c6ccfb806f86
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-06-26 22:57:54 -0300

    Create Comment.php

---
+++ .php/models/Comment.php
@@ -0,0 +1,52 @@
+<?php
+    class Comment {
+        private $id;
+        private $postId;
+        private $userId;
+        private $text;
+        private $dt;
+
+        /* CONSTRUTOR */
+        public function __construct($id, $postId, $userId, $text, $dt) {
+            $this->id = $id;
+            $this->postId = $postId;
+            $this->userId = $userId;
+            $this->text = $text;
+            $this->dt = $dt;
+        }
+
+        /* GETTERS */
+        public function getId() {
+            return $this->id;
+        }
+        public function getPostId() {
+            return $this->postId;
+        }
+        public function getUserId() {
+            return $this->userId;
+        }
+        public function getText() {
+            return $this->text;
+        }
+        public function getDt() {
+            return $this->dt;
+        }
+
+        /* SETTERS */
+        public function setId($id) {
+            $this->id = $id;
+        }
+        public function setPostId($postId) {
+            $this->postId = $postId;
+        }
+        public function setUserId($userId) {
+            $this->userId = $userId;
+        }
+        public function setText($text) {
+            $this->text = $text;
+        }
+        public function setDt($dt) {
+            $this->dt = $dt;
+        }
+    }
+?>
