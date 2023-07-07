e074e328ebe5a34c9a750d2c453721ea8bc29e3d
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-07-07 16:16:54 -0300

    07-07

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -83,3 +83,22 @@ $(document).on('click', '.btnPostLike', function() {
 
     toggleLikePost(postId, token, btn, likeNum);
 });
+
+$(document).on('click', '#follow', function() {
+    $.ajax({
+        type: "POST",
+        url: "php/api/createFollow.php",
+        dataType: "json",
+        data: {
+            user: user,
+            token: token
+        },
+        success: function(response) {
+            if (response.success == true) {
+                $("#follow").html('<i class="fa fa-user-plus"></i> Seguido');
+                $("#follow").removeClass('btn-outline-primary');
+                $("#follow").addClass('btn-primary');
+            }
+        }
+    });
+});
