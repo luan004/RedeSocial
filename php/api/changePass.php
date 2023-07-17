64dd7649814b7faa07eae8d90f027ecbc19fb5f9
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-07-17 14:24:32 -0300

    Create changePass.php

---
+++ php/api/changePass.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("../connection/Conn.php");
+    require_once('../dao/UserDAO.php');
+    require_once('../dao/sesstokenDAO.php');
+    require_once('../models/User.php');
+    require_once('../models/sesstoken.php');
+?>
