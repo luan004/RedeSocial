cab187a9d09810a2d7357db50bec2d615b5b755b
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-08-15 16:54:20 -0300

    Update teste.js

---
+++ teste.js
@@ -0,0 +1,19 @@
+/* document.write(`
+<script type='module'>
+    import api from '../../src/services/api.js';
+    const user = {
+        nome: 'luan004',
+        email: 'luan004@luan004.com',
+        password: '123456789',
+      };
+    const response = await api.put('tab_user', '', user);
+    console.log(api);
+</script>`); */
+
+const tes = async () => {
+    console.log('teste');
+};
+
+const teste = {tes};
+
+export default teste;
