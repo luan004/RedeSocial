11bff85e054311251ef54f26734501554342a2cd
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-11-07 14:13:54 -0300

    07-11

--- php/api/login.php
+++ php/api/login.php
@@ -5,12 +5,19 @@
     require_once('../models/User.php');
     require_once('../models/Sesstoken.php');
 
-    $user = $_POST['user'];
-    $pass = $_POST['pass'];
+    /* $user = $_POST['user'];
+    $pass = $_POST['pass']; */
+
+    $user = 'testandosenha';
+    $pass = '12345678';
 
     $conn = new Conn();
     $userDAO = new UserDAO($conn);
     $userObj = $userDAO->getUserByUsername($user);
+
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    echo $pass;
 
     if ($userObj && $userObj->getPass() === $pass) {
         $token = bin2hex(random_bytes(32)); //token hexadecimal de 64 caracteres
--- php/api/register.php
+++ php/api/register.php
@@ -3,9 +3,17 @@
     require_once('../dao/UserDAO.php');
     require_once('../models/User.php');
 
-    $name = $_POST['name'];
+    /* $name = $_POST['name'];
     $user = $_POST['user'];
-    $pass = $_POST['pass'];
+    $pass = $_POST['pass']; */
+   
+
+    $name = 'Teste';
+    $user = 'testanxdosenha';
+    $pass = '12345678';
+
+    $conn = new Conn();
+    $userDAO = new UserDAO($conn);
 
     switch (true) {
         case (strlen($name) > 64 || strlen($name) < 1):
@@ -14,13 +22,13 @@
                 'error' => 'nameInvalid'
             );
             break;
-        case (strlen($pass) > 32 || strlen($pass) < 8):
+        case (strlen($pass) > 32 || strlen($pass) < 8 || !preg_match('/^[a-zA-Z0-9_]+$/', $pass)):
             $response = array(
                 'register' => false,
                 'error' => 'passInvalid'
             );
             break;
-        case (strlen($user) > 32 || strlen($user) < 4 || !preg_match('/^[a-zA-Z0-9_]+$/', $user)):
+        case (strlen($user) > 32 || strlen($user) < 4 || !preg_match('/^[a-zA-Z0-9_]+$/', $user) || $userDAO->getUserByUsername($user)):
             $response = array(
                 'register' => false,
                 'error' => 'userInvalid'
@@ -30,8 +38,11 @@
             $conn = new Conn();
             $userDAO = new UserDAO($conn);
 
-            // sanitizacao
+            // sanitizacão
             $name = strip_tags($name);
+
+            // criptografia da senha
+            $pass = md5($pass);
             
             // Cria um novo usuário
             $userObj = new User(null, $name, $user, $pass, null, 'default', null, null, null);
